f7fe4d3b4a270ceea82d09b5e19fa33dbf68441a
lsw <w8734@naver.com>
2019-04-12 18:14:31 +0900

    first commit

---
+++ src/main/java/org/sangwon/servlets/example/HelloServlet.java
@@ -0,0 +1,26 @@
+package org.sangwon.servlets.example;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 데이터를 만든다.
+		String name = "이상원";
+
+		// 데이터를 request에 태운다.
+		request.setAttribute("name", name);
+
+		// /WEB-INF/jsp/hello.jsp로 forward 한다.
+		request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,
+				response);
+	}
+}
